667a6a451ee2b05df25a85e569f584fb5885edfe
kenny <411316753@qq.com>
2018-03-17 11:16:58 +0800

    add gitignore file to remove some usefuless file

--- src/main/java/com/thyme/controller/HomeController.java
+++ src/main/java/com/thyme/controller/HomeController.java
@@ -1,15 +1,20 @@
 package com.thyme.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @param :
+ * @author: kenny
+ * @Date: 2018/3/16-9:10
+ * @Description:
+ * @return:
+ */
 @Controller
-@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(){
-       return "index";
-    }
+  @RequestMapping(value="/home")
+  public String home(){
+    return "index";
+  }
 }
